b2d6a3c494efa4b9d89fc7b1331e32754a6ec320
GustavoBazan <99882976+GustavoBazan@users.noreply.github.com>
2022-05-23 10:12:17 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -8,4 +8,20 @@ module.exports = (app)=>{
     app.post('/login',(req,res)=>{
         res.render('atividades.ejs')
     })
+
+    // abrir a view atividades
+    app.post('/login',async(req,res)=>{
+        // conectar comobanco de dados
+        const conexao = require('../ config/database')()
+        // importar a model usuários
+        const usuarios = require('../models/usuarios')
+        //procurar pelo endereço de email
+        var procurar = await usuarios.findOne({email:body.email})
+        if(!procurar){
+            res.send('Email não cadastrado!!')
+        }
+
+        res.render('atividades.ejs')
+
+})
 }
