2483c29f4ff439a0f81b7feb1f8bc0f5b31ef576
GustavoBazan <99882976+GustavoBazan@users.noreply.github.com>
2022-06-06 10:08:56 -0300

    Add files via upload

--- routes/atividade.js
+++ routes/atividade.js
@@ -1,3 +1,5 @@
+const atividades = require('../models/atividades')
+
 module.exports = (app)=>{
     app.post('/atividades',async(req,res)=>{
         //recuperando as informações digitadas
@@ -14,12 +16,24 @@ module.exports = (app)=>{
             tipo:dados.tipo,
             entrega:dados.entrega,
             instrucoes:dados.orientacao,
+            disciplina:dados.disciplina,
             usuario:dados.id
+            
         }).save()
 
         //buscar todas as atividades desse usuário
         var buscar = await atividades.find({usuario:dados.id})
         console.log(buscar)
         res.render('atividades.ejs',{id:dados.id,nome:dados.nome,dados:buscar})
+    })
+
+    //excluir atividades
+    app.get("/excluir",async(req,res)=>{
+        //recuperar o parâmetro id da barra de endereço
+        var id = req.query.id
+        var excluir = await atividades.findOneAndRemove({_id:id})
+        //voltar para a página atividades
+        //res.render('atividades.ejs',{id:dados.id,nome:dados.nome,dados:buscar})
+        res.send("Atividade Excluída!!")
     })
 }
